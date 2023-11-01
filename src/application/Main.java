bf5efb7b958ccbce6f6c04cbe279c1f4b57a95bb
Vitor <vitorvvc2005@gmail.com>
2023-10-31 23:44:30 -0300

    Adicionando prints com os novos métodos

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,17 +12,24 @@ public class Main {
 
 
         Pessoa testeMeninoBom = new BonsMeninos("Estela", "123.456.789-10", "amo meu irmão");
-        String presenteUm = "Carro pro meu irmão lindo";
-        String presenteDois = "PS5 pro meu irmão lindo";
-        String presenteTres = "Um mac mini pro meu irmão lindo";
+
+        String acao1 = "Dei um PS5 pro meu irmão lindo";
+        String acao2 = "Dei um MAC Mini para meu irmão lindo";
+        String acao3 = "Dei um carro para meu irmão lindo";
+
 
         Materias geografiaEstela = new Materias("Geografia", 10.0, 7.0, 6.0);
         Materias historiaEstela = new Materias("Historia", 8.5, 6.0, 7.0);
         Materias matematicaEstela = new Materias("Geografia", 10.0, 8.0, 5.0);
 
-        testeMeninoBom.adicionarPresentes(presenteUm);
-        testeMeninoBom.adicionarPresentes(presenteDois);
-        testeMeninoBom.adicionarPresentes(presenteTres);
+        testeMeninoBom.adicionarBoasAcoes(acao1);
+        testeMeninoBom.adicionarBoasAcoes(acao2);
+        testeMeninoBom.adicionarBoasAcoes(acao3);
+
+        testeMeninoBom.adicionarNotas(geografiaEstela);
+        testeMeninoBom.adicionarNotas(historiaEstela);
+        testeMeninoBom.adicionarNotas(matematicaEstela);
+
 
 
         System.out.println(noelTeste);
