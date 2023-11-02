baec5eb6d33bba2494b48d53d0834718b0dfd33e
Vitor <vitorvvc2005@gmail.com>
2023-11-01 23:50:02 -0300

    Modificacoes na classe acoes

--- src/entities/Acoes.java
+++ src/entities/Acoes.java
@@ -1,2 +1,18 @@
-package entities;public class Acoes {
+package entities;
+
+public class Acoes {
+
+    private String acao;
+
+    public Acoes(String acao) {
+        this.acao = acao;
+    }
+
+    public String getAcao() {
+        return acao;
+    }
+
+    public void setAcao(String acao) {
+        this.acao = acao;
+    }
 }
