c0d49232daec7e0a7a6351b057590e81d4bd1a12
Vitor <vitorvvc2005@gmail.com>
2023-11-05 23:03:26 -0300

    Inicio da reescrita de classe materiais visando mais fidelidade com sistema escolar

--- src/entities/Materias.java
+++ src/entities/Materias.java
@@ -1,5 +1,6 @@
 package entities;
 
+import entitiesEnum.Etapa;
 import entitiesEnum.MateriasEnum;
 import entitiesEnum.Status;
 
@@ -7,53 +8,48 @@ import static entities.Pessoa.validarDouble;
 
 public class Materias {
     // Classe materias que são materias escolares.
-    // Recebendo uma classe enum de máterias até o 4 ano do fundamental, notas da primeira, segunda, terceira prova e uma classe enum de status
+    // Comecei a reescrever a classe considerando o método avaliativo de onde minha irmã estuda ( 7 De Setembro )
+
+    // Atributos "obrigatórios"
     private MateriasEnum materias;
-    private double av1;
-    private double av2;
-    private double av3;
+    private Etapa etapa;
+    private double parcial;
+    private double global;
+
+    // Atributos "extras"
+    private Double avFinal;
     private Status status;
 
     // Construtor que recebe uma materia e suas notas do semestre
-    public Materias(MateriasEnum materias, double av1, double av2, double av3) {
+    public Materias(MateriasEnum materias, double parcial, double global) {
         this.materias = materias;
         // Valida as duas primeiras notas
-        this.av1 = validarDouble(av1);
-        this.av2 = validarDouble(av2);
-        // Se for possivel ir para a av3. Atribuir o status aprovado e que a nota e a nota de av3 antes  escrita
-        if (podeIrParaAv3(av1, av2)) {
-            this.av3 = validarDouble(av3);
+        this.parcial = validarDouble(parcial);
+        this.global = validarDouble(global);
+        // Se for possivel continuar o percusso pelo colégio ele está "aprovado"
+        if (podeIrParaAv3(parcial, global)) {
             this.status = Status.APROVADO;
         } else {
+            // Se não está em uma breve recuperacao, sujeito a um teste para repor sua menor nota pela nova nota "avfinal"
             this.status = Status.RECUPERACAO;
         }
     }
+    // Método para substituir uma nota caso a mesma seja abaixo de 7 - No colegio onde minha irmã estuda existe o calculo para saber se sua
+    // nota na matéria é o bastante para passar. Aqui vai a sua formula ( E = Etapa ) - Nota da E = Parcial + Global
+    // ( 1xE1 + 2xE2 + 3xE3 + 4xE4 ) / 10 > 7 == Aprovado; Se a condicao anterior for "false" é recuperação
+
 
     // Calculo de sim ou não para retornar se o aluno esta apto para ir ou não à prova final
     public boolean podeIrParaAv3(double av1, double av2) {
         if ((av1 + av2) / 2 < 7.0) {
             return false;
+
         }
         return true;
     }
 
-    // Método para "fazer" a recuperação
-    public boolean recuperacao(double av4) {
-        validarDouble(av4);
-        if (podeIrParaAv3(getAv1(), getAv2())) {
-            throw new RuntimeException("Você já passou, não precisa ir para recuperação");
-        } else {
-            if (av4 >= 7 && av4 <= 10) {
-                this.status = Status.APROVADO;
-                return true;
-            } else {
-                this.status = Status.REPROVADO;
-                return false;
-            }
-        }
-    }
-
     // Gets e sets
+
     public MateriasEnum getMaterias() {
         return materias;
     }
@@ -62,28 +58,36 @@ public class Materias {
         this.materias = materias;
     }
 
-    public double getAv1() {
-        return av1;
+    public Etapa getEtapa() {
+        return etapa;
     }
 
-    public void setAv1(double av1) {
-        this.av1 = av1;
+    public void setEtapa(Etapa etapa) {
+        this.etapa = etapa;
     }
 
-    public double getAv2() {
-        return av2;
+    public double getParcial() {
+        return parcial;
     }
 
-    public void setAv2(double av2) {
-        this.av2 = av2;
+    public void setParcial(double parcial) {
+        this.parcial = parcial;
     }
 
-    public double getAv3() {
-        return av3;
+    public double getGlobal() {
+        return global;
     }
 
-    public void setAv3(double av3) {
-        this.av3 = av3;
+    public void setGlobal(double global) {
+        this.global = global;
+    }
+
+    public Double getAvFinal() {
+        return avFinal;
+    }
+
+    public void setAvFinal(Double avFinal) {
+        this.avFinal = avFinal;
     }
 
     public Status getStatus() {
