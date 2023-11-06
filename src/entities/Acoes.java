6f826205680615cf0f9368da9dab919754e32ce9
Vitor <vitorvvc2005@gmail.com>
2023-11-05 23:08:40 -0300

    Adicionando comentários a acoes

--- src/entities/Acoes.java
+++ src/entities/Acoes.java
@@ -2,12 +2,15 @@ package entities;
 
 public class Acoes {
 
+    // Classe ação que foi criada apenas para poder instanciar uma lista de ações de forma mais organizada
     private String acao;
 
+    // Construtor
     public Acoes(String acao) {
         this.acao = acao;
     }
 
+    // Get & Set
     public String getAcao() {
         return acao;
     }
