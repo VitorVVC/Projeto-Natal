aac993e7285ef9f03c4074779fbafe761b9f8095
Vitor <vitorvvc2005@gmail.com>
2023-11-05 23:04:04 -0300

    Correcao de métodos predefinidos de materias sabendo que seu construtor foi modificado

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,9 +17,9 @@ public class Main {
         Acoes acao2 = new Acoes("Dei um MAC Mini para meu irmão lindo");
         Acoes acao3 = new Acoes("Dei um carro para o meu irmão lindo");
 
-        Materias geografiaEstela = new Materias(MateriasEnum.GEOGRAFIA, 10.0, 7.0, 6.0);
-        Materias historiaEstela = new Materias(MateriasEnum.MATEMATICA, 8.5, 6.0, 7.0);
-        Materias matematicaEstela = new Materias(MateriasEnum.CIENCIA, 10.0, 8.0, 5.0);
+        Materias geografiaEstela = new Materias(MateriasEnum.GEOGRAFIA, 10.0, 7.0);
+        Materias historiaEstela = new Materias(MateriasEnum.MATEMATICA, 8.5, 6.0);
+        Materias matematicaEstela = new Materias(MateriasEnum.CIENCIA, 10.0, 8.0);
 
         ((BonsMeninos) testeMeninoBom).adicionarBoasAcoes(acao1);
         ((BonsMeninos) testeMeninoBom).adicionarBoasAcoes(acao2);
@@ -36,9 +36,9 @@ public class Main {
         Acoes acoes02 = new Acoes("Não dei um MAC Mini para meu irmão lindo");
         Acoes acoes03 = new Acoes("Não dei um carro para meu irmão lindo");
 
-        Materias inglesEstela = new Materias(MateriasEnum.INGLES, 5.0, 4.0, 6.0);
-        Materias portuguesEstela = new Materias(MateriasEnum.PORTUGUES, 6.5, 6.0, 5.0);
-        Materias redacaoEstela = new Materias(MateriasEnum.REDACAO, 7.0, 8.0, 4.0);
+        Materias inglesEstela = new Materias(MateriasEnum.INGLES, 5.0, 4.0);
+        Materias portuguesEstela = new Materias(MateriasEnum.PORTUGUES, 6.5, 6.0);
+        Materias redacaoEstela = new Materias(MateriasEnum.REDACAO, 7.0, 8.0);
 
         ((MausMeninos) testeMauMenino).adicionarMasAcoes(acoes01);
         ((MausMeninos) testeMauMenino).adicionarMasAcoes(acoes02);
@@ -51,7 +51,7 @@ public class Main {
 
         // System.out.println(noelTeste);
         // System.out.println(testeMeninoBom);
-        // System.out.println(testeMauMenino);
+        System.out.println(testeMauMenino);
 
 
     }
